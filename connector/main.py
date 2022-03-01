7a5b6dedf9f474d7d3350b2767e1338c32f709a3
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2022-03-01 19:14:52 +0100

    get formatted data

--- connector/main.py
+++ connector/main.py
@@ -1,16 +1,21 @@
+from xml.etree.ElementTree import fromstring
+
 from fastapi import FastAPI
 from requests import Session
 from zeep import Transport, Client
 
 app = FastAPI()
 
-
 SANDBOX_URL = 'https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/wsdl/UslugaBIRzewnPubl-ver11-test.wsdl'
 SANDBOX_PASSWORD = 'abcde12345abcde12345'
 
 
+def get_formatted_data(data):
+    return {info.tag: "" if info.text is None else info.text for info in fromstring(data)[0]}
+
+
 @app.get("/{nip}")
-async def get_nip(nip: int):
+async def get_company(nip: int):
     headers = {}
     transport = Transport(session=Session())
     transport.session.headers = headers
@@ -21,5 +26,4 @@ async def get_nip(nip: int):
     headers.update({"sid": session_key})
     data = client.service.DaneSzukajPodmioty(pParametryWyszukiwania={'Nip': nip})
     client.service.Wyloguj(pIdentyfikatorSesji=session_key)
-    return data
-
+    return get_formatted_data(data)
