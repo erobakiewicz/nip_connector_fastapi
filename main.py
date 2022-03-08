447d1df6b58218528f1d9dc2e2a4102e2e2bb746
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2022-03-08 17:58:18 +0100

    Added all CRUD views for comapnies

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from xml.etree.ElementTree import fromstring
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from requests import Session as request_session
 from sqlalchemy.orm import Session as sql_session
 from zeep import Transport, Client
@@ -8,6 +8,7 @@ from zeep import Transport, Client
 from src import models
 from src.database import SessionLocal
 from src.models import Company
+from src.schemas import Company as company_schema
 
 app = FastAPI()
 
@@ -58,6 +59,31 @@ async def create_company(nip: int, db: sql_session = Depends(get_db)):
     return db_company
 
 
+@app.put("/update/{company_id}")
+async def update_company(company_id: int, company: company_schema, db: sql_session = Depends(get_db)):
+    company_model = db.query(models.Company).filter(models.Company.id == company_id).first()
+    if not company_model:
+        raise http_exception()
+    company_model.nip = company.nip
+    company_model.regon = company.regon
+    company_model.name = company.name
+
+    db.add(company_model)
+    db.commit()
+
+    return successful_response(200)
+
+
+@app.delete("/delete/{company_id")
+async def delete_company(company_id: int, db: sql_session = Depends(get_db)):
+    company_model = db.query(models.Company).filter(models.Company.id == company_id).first()
+    if not company_model:
+        raise http_exception()
+    db.query(models.Company).filter(models.Company.id == company_id).delete()
+    db.commit()
+    return successful_response(200)
+
+
 @app.get("/companies/")
 async def get_all_companies(db: sql_session = Depends(get_db)):
     return db.query(models.Company).all()
@@ -66,3 +92,11 @@ async def get_all_companies(db: sql_session = Depends(get_db)):
 @app.get("/companies/{nip}")
 async def get__company(nip: int, db: sql_session = Depends(get_db)):
     return db.query(models.Company).filter(models.Company.nip == nip).first()
+
+
+def successful_response(status_code: int):
+    return {'status': status_code, 'transaction': 'Successful'}
+
+
+def http_exception():
+    return HTTPException(status_code=404, detail="Item not found")
